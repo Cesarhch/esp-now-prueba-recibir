09f957f08b839cd61853c2292b4fa6c6306d7d7b
Cesarhch <cesarhch1@gmail.com>
2023-01-26 17:17:20 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,77 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
+
+uint8_t broadcastAddress[] = {0x30, 0xAE, 0xA4, 0x04, 0x10, 0x3C};
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+  int a;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myDataRec;
+struct_message myDataSen;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  
+}
+ 
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myDataRec, incomingData, sizeof(myDataRec));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Int: ");
+  Serial.println(myDataRec.a);
+  Serial.println();
+}
+
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(9600);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  //Este apartado del peer es util para enviar el paquete
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer
+          
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+  // Set values to send
+  myDataSen.a = 1;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myDataSen, sizeof(myDataSen));
+  
+  delay(2000);
+}
